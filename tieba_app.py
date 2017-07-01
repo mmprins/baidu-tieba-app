7ce97ae516e13ecdd4e31845254967d102adef14
mmprins <janesyya@gmail.com>
2017-07-01 22:35:32 +0800

    start with browse only

---
+++ tieba_app.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import json,os
+import http.cookiejar
+import requests
+from html.parser import HTMLParser
+from urllib import parse
+class BaiduTieba(object):
+    LOGIN_ERR_MSGS = {
+    "1": "用户名格式错误，请重新输入",
+    "2": "用户不存在",
+    "3": "",
+    "4": "登录密码错误，请重新输入",
+    "5": "今日登陆次数过多",
+    "6": "验证码不匹配，请重新输入验证码",
+    "7": "登录时发生未知错误，请重新输入",
+    "8": "登录时发生未知错误，请重新输入",
+    "16": "对不起，您现在无法登陆",
+    "51": "改手机号未通过验证",
+    "52": "该手机已经绑定多个用户",
+    "53": "手机号码格式不正确",
+    "58": "手机号格式错误，请重新输入",
+    "256": "",
+    "257": "请输入验证码",
+    "20": "此账号已登录人数过多",
+    "default": "登录时发生未知错误，请重新输入"
+    }
+    POST_ERR_MSGS = {
+    "38": "验证码超时，请重新输入",
+    "40": "验证码输入错误，请您返回后重新输入",
+    "703": "为了减少恶意灌水和广告贴，本吧被设置为仅本吧会员才能发帖",
+    "704": "为了减少恶意灌水和广告贴，本吧被设置为仅本吧管理团队才能发帖，给您带来的不便表歉意",
+    "705": "本吧当前只能浏览，不能发帖！",
+    "706": "抱歉，本帖暂时无法回复。",
+    "900": "为抵御挖坟威海，本吧吧主已放出贴吧神兽--超级静止蛙，本帖暂时无法回复。",
+    }
+    CookiePath=os.path.expanduser('~')+'/baidu-cookies'
+    Referer='https://tieba.baidu.com'
+    LOGIN_URL="http://passport.baidu.com/?login"
+    LOGIN_IMG_URL="http://passport.baidu.com/?verifypic"
+    POST_URL="http//tieba.baidu.com/f/commit/thread/add"
+    THREAD_URL="http://tieba.baidu.com/f/commit.thread/add"
+    TBS_URL="http://tieba.baidu.com/dc/common/tbs"
+    VCODE_URL="http://tieba.baidu.com/f/user/json_vocde? lm=%s&rs10=2&rs1=0&t=0.7"
+    IMG_URL="http://tieba.baidu.com/cgi-bin/genimg?%s"
+    title_dict={}
+    def __init__(self,user=None,passwd=None):
+        self.user=user
+        self.passwd=passwd
+        self.loginTest()
+    def loginTest(self):
+        pass
+    def user_agent(self,agent):
+        with open('user-agent.json') as fp:
+            return json.load(fp)[agent]
+    def Mozilla_Firebug_cookies_checker(self,path='/cookies.txt'):
+        cookie='# Netscape HTTP Cookie File\n'
+        with open(self.CookiePath+path,'r') as fp:
+            for line in fp.readlines():
+                if line.split('\t')[4].isnumeric():
+                    cookie+=line
+        return cookie
+    def mozilla_cookies_resave(self):
+        try:
+            with open(self.CookiePath+'/mozilla-cookies.txt','w') as fp:
+                fp.write(self.Mozilla_Firebug_cookies_checker())
+                print('Mozilla cookies saved!')
+        except:
+            print('cookies file discorrect!')
+    def Session(self,agent="Ie"):
+        S=requests.Session()
+        cj=http.cookiejar.MozillaCookieJar(self.CookiePath+'/mozilla-cookies.txt')
+        cj.load()
+        S.cookies.update(cj)
+        S.headers.update({'User-Agent':self.user_agent(agent)})
+        return S
+    def TiebaList(self,tieba_name,agent="Ie"):
+        url='https://tieba.baidu.com/f?kw=%s&fr=index'%tieba_name
+        self.title_dict={}
+        req=self.Session(agent).get(url)
+        P=htmlparser()
+        P.title_dict={}
+        P.feed(req.text)
+        self.title_dict=P.title_dict
+    def reply(self,index,ptype='reply',*args):
+        pass
+    def SaverHtml(self,req):
+        with open('/tmp/samp.html','w') as fp:
+            fp.write(req.text)
+            print('save to /tmp/samp.html')
+    pass
+class htmlparser(HTMLParser):
+    count,title_dict=1,{}
+    def title_clear(self):
+        self.title_dict={}
+    def handle_starttag(self,tag,attrs):
+        if tag == 'a':
+            for name,value in attrs:
+                if name == 'class':
+                    if value == ' card_title_fname':
+                        for name,value in attrs:
+                            if name == 'href':
+                                print('贴吧名称：%s'%(parse.unquote(value)))
+                    if value == 'j_th_tit ':
+                        print('%d.%s=%s;%s=%s'%(self.count,attrs[0][0],attrs[0][1],attrs[1][0],attrs[1][1]))
+                        self.title_dict[self.count]=(attrs[0][1][3:13],attrs[1][1])
+                        self.count+=1
+if __name__ == '__main__':
+    pass
